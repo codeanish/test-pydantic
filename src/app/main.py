c000b9a95dbafcccb471df72ceec954df79a5be8
codeanish <anish@codeanish.com>
2023-10-26 20:45:05 -0700

    Testing out validation

---
+++ src/app/main.py
@@ -0,0 +1,6 @@
+from models.user import User
+
+
+user = User(name="John Doe", email = "bob@example.com", age=19)
+
+print(user)
---
+++ tests/test_user.py
@@ -0,0 +1,26 @@
+from pydantic import ValidationError
+import pytest
+from src.app.models.user import User
+
+
+def test_user_empty_email():
+    with pytest.raises(ValidationError):
+        User(name="John Doe", email = "", age=19)
+
+def test_user_null_email():
+    with pytest.raises(ValidationError):
+        User(name="John Doe", age=19)
+
+def test_user_too_young():
+    with pytest.raises(ValidationError):
+        User(name="John Doe", email = "test@example.com", age=17)
+    
+def test_valid_user():
+    user = User(name="John Doe", email = "test@example.com", age=19)
+    assert user.age == 19
+    assert user.name == "John Doe"
+    assert user.email == "test@example.com"
+
+def test_user_invalid_name():
+    with pytest.raises(ValidationError):
+        User(name="Jo", email = "test@example.com", age=19)
